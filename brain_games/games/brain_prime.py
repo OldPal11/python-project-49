977fcd7f0879ebcca5fcb8405b7cde8ee90f3c71
OldPal11 <oldpal11.es@gmail.com>
2025-01-25 11:21:10 +0100

    correct brain_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -9,8 +9,16 @@ def game_rules():
     return 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+
 def generate_round():
     random_number = random.randint(MINIMAL_PRIME_NUMBER, RANDOM_MAX)
-    is_prime = all(random_number % i != 0 for i in range(2, random_number))
     correct_answer = 'yes' if is_prime(random_number) else 'no'
     return random_number, correct_answer
